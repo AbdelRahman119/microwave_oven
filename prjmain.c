6bea1ea1aae4c365fc4df82b688f3f91eb09ba42
OmarBadr108 <105713653+OmarBadr108@users.noreply.github.com>
2022-05-18 16:37:47 +0200

    Update prjmain.c
    i have finihsed system_Init

--- prjmain.c
+++ prjmain.c
@@ -128,7 +128,23 @@ lcdoutput("Beef");
 
 }
 	
-	
+	// system initilization 
+		void SystemInit(void){
+
+SYSCTL_RCGCGPIO_R |= 0x20; // PortF clock enable
+while ((SYSCTL_PRGPIO_R & 0x20)==0); //Delay
+GPIO_PORTF_LOCK_R = 0x4C4F434B; // Unlock PortF Commit register
+GPIO_PORTF_CR_R |= 0x1F; // Allow changes to PF
+GPIO_PORTF_AMSEL_R &= ~(0x1F); // Disable analog function
+GPIO_PORTF_PCTL_R &= ~0x000FFFFF; // GPIO clear bit PCTL
+GPIO_PORTF_AFSEL_R &= ~(0x1F); // No alternate function
+GPIO_PORTF_DIR_R |= 0x0E; // PF321 output
+GPIO_PORTF_DEN_R |= 0x1F; // Enable digital pins PF4-PF0
+GPIO_PORTF_PUR_R |= 0x11;	
+GPIO_PORTF_DATA_R &= ~(0x0E); // Initialize LEDs to be off
+}
+
+
 	
 	
 	
