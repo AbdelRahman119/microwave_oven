b080c8e0b06beaee81de259f650a37824838300a
RaghdaElattar10 <105421571+RaghdaElattar10@users.noreply.github.com>
2022-05-19 21:24:25 +0200

    Update systic.c
    final systick after reviewing

--- systic.c
+++ systic.c
@@ -7,23 +7,32 @@ void SysTick_wait (unsigned int delay)
 NVIC_ST_CTRL_R=0x00; //disable systick
 NVIC_ST_RELOAD_R=delay-1; //reload value
 NVIC_ST_CURRENT_R=0; //clear current value
-NVIC_ST_CTRL_R=0x05; //enable systick 	
+NVIC_ST_CTRL_R=0x00000005; //enable systick 	
 
 while((NVIC_ST_CTRL_R&0x00010000)==0){}//wait for count flag;
 } 
 
 /////////////////////////////////////////////// 
 
-void Delay_1ms (unsigned int multi_time)
+void delay_inMilli (unsigned int delay)
 {
 int i;
-for(i=0; i<multi_time ; i++)
+for(i=0; i<delay ; i++)
 {
-SysTick_wait(16000-1);	//1ms
+SysTick_wait(80000/5);	//1ms
 }
 }
 
+
 /////////////////////////////////////////////// /////////////////////////////////////////////// 
+void delay_inMicro (unsigned int delay)
+{
+int i;
+for(i=0; i<delay ; i++)
+{
+SysTick_wait(16);	//1 microsec
+}
+}
 int main()
 {
 Delay_1ms (15);
